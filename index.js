3c2247761127ff8ee7405040af9edf78d1f43237
Rizalnj <rizal.njnew@gmail.com>
2020-09-18 13:48:39 +0700

    lanjut

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 
 const app = express();
-app.use(() => {
-    console.log('hello server...');
+const router = express.Router();
+
+router.use( '/products', (req, res, next) => {
+    res.json({name: "rizal", email: "rznj@gmail.com"});
+    next();
 })
+
+router.use('/price', (req, res, next) => {
+    res.json({price: 25000});
+    next();
+})
+
+router.get('/customers', (req, res, next) => {
+    res.json({title: "Customer"});
+    next();
+})
+
+app.use('/', router);
+
+
+// GET '/users' ==> [{name}]
 
 app.listen(4000);
